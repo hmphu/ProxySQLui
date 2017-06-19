fb306afcf0faaf5b4e6d9ee40af9013b1c315554
Tian, Lei <taylor840326@gmail.com>
2017-06-19 18:21:06 +0800

    Add servers servers

---
+++ src/services/servers.js
@@ -0,0 +1,61 @@
+import request from '../utils/request';
+import { PAGE_SIZE } from '../constants';
+
+export function ListAllServers({ page }) {
+  return request(`/api/servers?_page=${page}&_limit=${PAGE_SIZE}`);
+}
+
+export function ListServerByHostgroup(hostgroupid) {
+  return request(`/api/servers/${hostgroupid}`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'GET',
+  });
+}
+
+export function ListOneServer(values) {
+  return request('/api/servers', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify(values),
+  });
+}
+
+export function CreateOneServer(values) {
+  return request('/api/servers', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify(values),
+  });
+}
+
+export function UpdateOneServerStatus(values) {
+  return request('/api/servers/status', {
+    header: { 'Content-Type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify(values),
+  });
+}
+
+export function UpdateOneServerWeight(values) {
+  return request('/api/servers/weight', {
+    header: { 'Content-Type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify(values),
+  });
+}
+
+export function UpdateOneServerMC(values) {
+  return request('/api/servers/maxconnection', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify(values),
+  });
+}
+
+export function DeleteOneServers(values) {
+  return request('/api/servers', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'DELETE',
+    body: JSON.stringify(values),
+  });
+}
